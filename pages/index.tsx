960968a6b3228ae584f380369c2a84cc39a1da1c
desperado1802 <mumun_ali@hotmail.com>
2023-04-30 16:30:48 +0300

    created protection to the home page by checking session

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,35 @@
+import useCurrentUser from "@/hooks/useCurrentUser";
+import { NextPageContext } from "next";
+import { signOut, getSession } from "next-auth/react";
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false,
+      },
+    };
+  }
+  return {
+    props: {},
+  };
+}
+
 export default function Home() {
+  const { data: user } = useCurrentUser();
+  console.log(user);
+
   return (
     <>
       <h1 className="text-2xl text-green-200">Netflix Clone</h1>
+      <p className="text-white">Logged in as {user?.name}</p>
+
+      <button className="h-10 w-full bg-white" onClick={() => signOut()}>
+        Logout
+      </button>
     </>
   );
 }
