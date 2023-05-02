4974d8eb643cbc55ec7f981af3b3258debcdaf21
desperado1802 <mumun_ali@hotmail.com>
2023-05-02 13:00:13 +0300

    created the navbar component

---
+++ components/NavbarItem.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+interface NavbaritemProps {
+  label: string;
+}
+
+const NavbarItem: React.FC<NavbaritemProps> = ({ label }) => {
+  return (
+    <div className="text-white cursor-pointer hover:text-gray-300 transition">
+      {label}
+    </div>
+  );
+};
+
+export default NavbarItem;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
+import Navbar from "@/components/Navbar";
 import useCurrentUser from "@/hooks/useCurrentUser";
 import { NextPageContext } from "next";
-import { signOut, getSession } from "next-auth/react";
+import { getSession } from "next-auth/react";
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -20,16 +21,10 @@ export async function getServerSideProps(context: NextPageContext) {
 
 export default function Home() {
   const { data: user } = useCurrentUser();
-  console.log(user);
 
   return (
     <>
-      <h1 className="text-2xl text-green-200">Netflix Clone</h1>
-      <p className="text-white">Logged in as {user?.name}</p>
-
-      <button className="h-10 w-full bg-white" onClick={() => signOut()}>
-        Logout
-      </button>
+      <Navbar />
     </>
   );
 }
